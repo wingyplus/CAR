6a5994dbba25ab99fb8f98cbb954c151ddd6ed3b
Thanabodee <wingyminus@gmail.com>
2011-10-25 02:43:43 +0700

    edit Student form

--- CAR/ManageForm.vb
+++ CAR/ManageForm.vb
@@ -1,6 +1,7 @@
 ﻿Imports CarClassLibrary
 
 Public Class ManageForm
+    Private IdTmp As String
 
     Private Sub ClearStudentForm()
         IDName_TextBox.Text = ""
@@ -9,6 +10,7 @@ Public Class ManageForm
         Dorm_TextBox.Text = ""
         Field_ComboBox.Text = ""
         Phone_TextBox.Text = ""
+        IDName_TextBox.Focus()
     End Sub
 
     Private Sub BindDataSource()
@@ -48,26 +50,66 @@ Public Class ManageForm
     ' นำข้อมูลจาก Field ต่างๆ ลงฐานข้อมูล
     '
     Private Sub StudentSave_Button_Click(sender As System.Object, e As System.EventArgs) Handles StudentSave_Button.Click
-        Try
-            Dim student As New Student
-            student.Id = IDName_TextBox.Text.Trim
-            student.FirstName = FName_TextBox.Text
-            student.LastName = LName_TextBox.Text
-            student.Dome = Dorm_TextBox.Text
-            student.StudyField = Field_ComboBox.Text
-            student.Phone = Phone_TextBox.Text
+        If StudentSave_Button.Text = "แก้ไขข้อมูล" Then
             Using entity As New DataCarEntities
-                entity.Students.AddObject(student)
+                '
+                ' Get Student Objects with Id
+                '
+                Dim student = entity.Students.First(
+                    Function(st) st.Id = IdTmp
+                        )
+
+                student.Id = IDName_TextBox.Text
+                student.FirstName = FName_TextBox.Text
+                student.LastName = LName_TextBox.Text
+                student.Dome = Dorm_TextBox.Text
+                student.StudyField = Field_ComboBox.Text
+                student.Phone = Phone_TextBox.Text
+
                 entity.SaveChanges()
+                MsgBox("แก้ไขข้อมูลเรียบร้อย")
+
+                '
+                ' เปลี่ยนชื่อปุ่มกลับ
+                '
+                StudentSave_Button.Text = "บันทึกข้อมูล"
+                ClearStudentForm()
+                IDName_TextBox.Enabled = True
             End Using
-        Catch ex As UpdateException
-            MsgBox("ข้อมูลนี้มีอยู่แล้ว")
-        Catch ex As Exception
-            MsgBox(ex.Message)
-        End Try
-        MsgBox("บันทึกข้อมูลเรียบร้อย")
-        StudentOwner_ComboBox.Items.Add(IDName_TextBox.Text & "-" & FName_TextBox.Text & " " & LName_TextBox.Text)
-        ClearStudentForm()
+        Else
+            Try
+                '
+                ' สร้าง Object Student และทำการเก็บลงฐานข้อมูล
+                '
+                Dim student As New Student
+                student.Id = IDName_TextBox.Text
+                student.FirstName = FName_TextBox.Text
+                student.LastName = LName_TextBox.Text
+                student.Dome = Dorm_TextBox.Text
+                student.StudyField = Field_ComboBox.Text
+                student.Phone = Phone_TextBox.Text
+
+                Using entity As New DataCarEntities
+                    '
+                    ' Add Object to memory
+                    '
+                    entity.Students.AddObject(student)
+                    entity.SaveChanges()
+                End Using
+                MsgBox("บันทึกข้อมูลเรียบร้อย")
+            Catch ex As UpdateException
+                MsgBox("ข้อมูลนี้มีอยู่แล้ว")
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+            StudentOwner_ComboBox.Items.Add(IDName_TextBox.Text & "-" & FName_TextBox.Text & " " & LName_TextBox.Text)
+            ClearStudentForm()
+        End If
+
+        '
+        ' Update Data Table
+        '
         BindDataSource()
     End Sub
 
@@ -79,17 +121,21 @@ Public Class ManageForm
     Private Sub StudentDataGridView_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles StudentDataGridView.CellContentClick
         Dim index = StudentDataGridView.CurrentRow.Index
         Dim id = CStr(StudentDataGridView.Item(0, index).Value)
+
         Using entity As New DataCarEntities
             Dim student = entity.Students.First(
                 Function(st) st.Id = id
                     )
-
+            IdTmp = student.Id
             IDName_TextBox.Text = student.Id
             FName_TextBox.Text = student.FirstName
             LName_TextBox.Text = student.LastName
             Dorm_TextBox.Text = student.Dome
             Field_ComboBox.SelectedText = student.StudyField
             Phone_TextBox.Text = student.Phone
+            StudentSave_Button.Text = "แก้ไขข้อมูล"
         End Using
+
+        IDName_TextBox.Enabled = False
     End Sub
 End Class
