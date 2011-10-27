77916e2cc0a6c0032e131766ee30a71e0c4d6e9d
Thanabodee <wingyminus@gmail.com>
2011-10-27 23:47:30 +0700

    set tab index in car tab

--- CAR/ManageForm.vb
+++ CAR/ManageForm.vb
@@ -6,9 +6,9 @@ Public Class ManageForm
         Select Case Bind
             Case "Students"
                 Using dataCar As New DataCarEntities
-                    Dim students = From a In dataCar.Students
+                    Dim students = (From a In dataCar.Students
                                    Select a.Id, a.FirstName, a.LastName
-                                   Order By Id Ascending
+                                   Order By Id Ascending).ToList()
                     StudentBindingSource.DataSource = students
                 End Using
             Case "Cars"
@@ -31,9 +31,9 @@ Public Class ManageForm
     '
     Private Sub ManageForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         Using dataCar As New DataCarEntities
-            Dim students = From a In dataCar.Students
+            Dim students = (From a In dataCar.Students
                            Select a.Id, a.FirstName, a.LastName
-                           Order By Id Ascending
+                           Order By Id Ascending).ToList()
             StudentBindingSource.DataSource = students
             For Each student In students
                 StudentOwner_ComboBox.Items.Add(student.Id & "-" & student.FirstName & " " & student.LastName)
